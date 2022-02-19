bf4712ea1b5e459e81c96d2136b7682bb9ea0477
Cristiano <cristianodevsystem@gmail.com>
2022-02-19 18:41:13 -0300

    :up: Criação do `nest g controller users --no-spec` e retorno da rota no `dtos`

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,9 +2,11 @@ import { UserRepository } from './repository/users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserRepository])],
     providers: [UsersService],
+    controllers: [UsersController],
 })
 export class UsersModule {}
