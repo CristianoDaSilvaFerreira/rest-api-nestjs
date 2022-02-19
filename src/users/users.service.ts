cd22b959cb9cdafd4bb25f22bf25b9e16c094944
Cristiano <cristianodevsystem@gmail.com>
2022-02-19 18:38:24 -0300

    :up: Chamando o método `createAdminUser`no `users.service.ts`

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
+import { UserRole } from './Enum/users.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dtos/create-user.dto';
 import { UserRepository } from './repository/users.repository';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
@@ -8,4 +11,13 @@ export class UsersService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  //   Método de criação de usuários
+  async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+    }
+  }
 }
