44497ceaf7c4367c2085d3385cdfc57053b66386
Cristiano <cristianodevsystem@gmail.com>
2022-02-19 19:19:58 -0300

    :up: Chamando o método de `signIn`no `auth.service.ts`

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
+import { CredentialsDto } from './../users/dtos/credentials.dto';
 import { UserRole } from './../users/Enum/users.service';
 import { User } from './../users/entities/user.entity';
 import { CreateUserDto } from './../users/dtos/create-user.dto';
 import { UserRepository } from './../users/repository/users.repository';
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
@@ -18,6 +19,15 @@ export class AuthService {
       throw new UnprocessableEntityException('As senhas não conferem');
     } else {
       return await this.userRepository.createUser(createUserDto, UserRole.USER);
+    }
+  }
+
+  //   Método de criação de usuário com credenciais
+  async signIn(credentialsDto: CredentialsDto) {
+    const user = await this.userRepository.checkCredentials(credentialsDto);
+
+    if (user === null) {
+      throw new UnauthorizedException('Credenciais inválidas');
     }
   }
 }
