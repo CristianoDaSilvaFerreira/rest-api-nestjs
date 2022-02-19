c9995427936aeed6be86a9281b8a3a012f3c72da
Cristiano <cristianodevsystem@gmail.com>
2022-02-19 18:50:25 -0300

    :rocket: Endpoint `Post() - createAdminUser` finalizado

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { ReturnUserDto } from './dtos/return-users.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
+import { Body, Controller, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  // Endpoint de criação de usuários administrador 
+  @Post()
+  async createAdminUser(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
+    return {
+      user,
+      message: 'Administrador cadastrado com sucesso',
+    };
+  }
+}
